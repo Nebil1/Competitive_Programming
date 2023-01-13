46d79e823a409552a3355f9b289564caea469c96
Nebil1 <nebilyk@gmail.com>
2023-01-13 14:59:47 +0300

    Add files

--- BubbleSort.py
+++ BubbleSort.py
@@ -4,14 +4,8 @@ import random
 import re
 import sys
 
-#
-# Complete the 'countSwaps' function below.
-#
-# The function accepts INTEGER_ARRAY a as parameter.
-#
 
 def countSwaps(a):
-    # Write your code here
     counter = 0
     n = len(a)
     for i in range(n-1):
@@ -25,9 +19,3 @@ def countSwaps(a):
     print("Array is sorted in " + str(counter) + " swaps.")
     print("First Element: " + str(a[0]))
     print("Last Element: " + str(a[n-1]))
-
-
-if __name__ == '__main__':
-    n = int(input().strip())
-    a = list(map(int, input().rstrip().split()))
-    countSwaps(a)
