4f6c2bb04feed074fae6be3258c69328b03a9318
Nebil1 <nebilyk@gmail.com>
2023-01-21 00:05:30 +0300

    Add solution to largestnumber

---
+++ 179_LargestNumber.py
@@ -0,0 +1,13 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        for i, num in enumerate(nums):
+            nums[i] = str(num)
+
+        def cmp(x, y):
+            if x+y > y+x:
+                return -1
+            else:
+                return 1
+
+        nums = sorted(nums, key=cmp_to_key(cmp))
+        return str(int("".join(nums)))
