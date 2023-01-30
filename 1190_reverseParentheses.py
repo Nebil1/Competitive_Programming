91788b7523043a61b8845cb656b442848bc43a8c
Nebil1 <nebilyk@gmail.com>
2023-01-30 20:51:05 +0300

    Add files

---
+++ 1190_reverseParentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverseParentheses(self, s: str) -> str:
+        stack = []
+        for c in s:
+            if c == '(':
+                stack.append([])
+            elif  c == ')':
+                words = stack.pop()[::-1]
+                if stack:
+                    stack[-1] += words
+            else:
+                if stack:
+                    stack[-1].append(c)    
+                else:
+                    stack.append([c])    
+        return "".join(stack[-1])
