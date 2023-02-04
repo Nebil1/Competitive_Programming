9068b495bc03db5d3963b9655f739573daaf85a0
Nebil1 <nebilyk@gmail.com>
2023-02-04 23:16:15 +0300

    Add files

---
+++ 1823_findTheWinner.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        A = [i for i in range(1, n + 1)]
+        curr = 0
+        while len(A) > 1:
+            remove = A[(curr + k - 1) % len(A)]
+            curr = (curr + k - 1) % len(A)
+            A = [x for x in A if x != remove]
+        return A[0]
