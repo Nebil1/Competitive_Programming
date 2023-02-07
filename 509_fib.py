813417160772f7a4a6259b1f228c4758f86f8240
Nebil1 <nebilyk@gmail.com>
2023-02-07 08:15:41 +0300

    Add files

---
+++ 509_fib.py
@@ -0,0 +1,11 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n < 2:
+            return n
+        f0 = 0
+        f1 = 1
+        for i in range(2, n+1):
+            f2 = f1 + f0
+            f0 = f1
+            f1 = f2
+        return f1
