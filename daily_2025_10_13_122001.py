22122e4c1871ae037e647f2e92e10d2853dc0900
Nebil1 <nebilyk@gmail.com>
2025-10-13 12:20:02 +0300

    Practice - 2025-10-13 12:20

---
+++ daily_2025_10_13_122001.py
@@ -0,0 +1,14 @@
+# Daily coding practice - 2025_10_13 122001
+# Valid Parentheses - Check if string has valid parentheses
+
+# Solution:
+def is_valid(s):
+    stack = []
+    mapping = {')': '(', '}': '{', ']': '['}
+    for char in s:
+        if char in mapping:
+            if not stack or stack.pop() != mapping[char]:
+                return False
+        else:
+            stack.append(char)
+    return not stack
