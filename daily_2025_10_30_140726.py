fd54f6b4ceb886e031f6b391dc421f638de30335
Nebil1 <nebilyk@gmail.com>
2025-10-30 14:07:26 +0300

    Practice - 2025-10-30 14:07

---
+++ daily_2025_10_30_140726.py
@@ -0,0 +1,10 @@
+# Daily coding practice - 2025_10_30 140726
+# Reverse String - Reverse string in-place
+
+# Solution:
+def reverse_string(s):
+    left, right = 0, len(s) - 1
+    while left < right:
+        s[left], s[right] = s[right], s[left]
+        left += 1
+        right -= 1
